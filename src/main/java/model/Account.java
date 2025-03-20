adaaa7c806898d8fc2e2461eb9a2d4aa6e29803d
MateusChavito <mateus.chavito@hotmail.com>
2025-03-20 17:43:58 -0300

    feat: Iniciar implementação da camada de dominio

---
+++ src/main/java/model/Account.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.math.BigDecimal;
+
+public class Account {
+
+    private String number;
+    private String agency;
+    private BigDecimal balance;
+    private BigDecimal limit;
+
+}
