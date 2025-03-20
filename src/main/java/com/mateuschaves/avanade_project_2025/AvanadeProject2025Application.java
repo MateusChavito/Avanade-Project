9786026bbffda31b75ac79649e417b7febb26665
MateusChavito <mateus.chavito@hotmail.com>
2025-03-20 17:24:32 -0300

    Initial commit

---
+++ src/main/java/com/mateuschaves/avanade_project_2025/AvanadeProject2025Application.java
@@ -0,0 +1,13 @@
+package com.mateuschaves.avanade_project_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanadeProject2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvanadeProject2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateuschaves/avanade_project_2025/AvanadeProject2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateuschaves.avanade_project_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvanadeProject2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
