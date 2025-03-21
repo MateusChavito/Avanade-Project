e5eac8fd0e3a558ce5c9d35b57cc082c2f1cbac9
MateusChavito <mateus.chavito@hotmail.com>
2025-03-21 13:59:37 -0300

    Implementar controller e expor API's

--- src/main/java/com/mateuschaves/avanade_project_2025/domain/model/Card.java
+++ src/main/java/com/mateuschaves/avanade_project_2025/domain/model/Card.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 
-@Entity
+@Entity(name = "tb_card")
 public class Card {
 
     @Id
--- src/main/java/com/mateuschaves/avanade_project_2025/service/impl/UserServiceImpl.java
+++ src/main/java/com/mateuschaves/avanade_project_2025/service/impl/UserServiceImpl.java
@@ -17,7 +17,6 @@ public class UserServiceImpl implements UserService {
         this.userRepository = userRepository;
     }
 
-
     @Override
     public User findById(Long id) {
         return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
